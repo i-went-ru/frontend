8de97c5860801eb8da7e4718d9b0a2556b1d3b5b
1Suro1 <timu.zaxarov@gmail.com>
2023-11-22 09:10:54 +0300

    add docker ci

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -2,4 +2,8 @@ const path = require('path');
 module.exports = {
   reactStrictMode: true,
   transpilePackages: ["ui"],
+  output: "standalone",
+  experimental: {
+    outputFileTracingRoot: path.join(__dirname, "../../"),
+  },
 };
